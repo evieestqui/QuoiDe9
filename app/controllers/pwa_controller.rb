ddc9f7140eebaf2c770350002f4caaf6c2b6580f
Ivy_Chiang <ivychiang1109@gmail.com>
2025-04-03 15:46:35 +0200

    Add PWA setup

---
+++ app/controllers/pwa_controller.rb
@@ -0,0 +1,9 @@
+class PwaController < ApplicationController
+  skip_forgery_protection
+
+  def service_worker
+  end
+
+  def manifest
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,26 @@ Rails.application.routes.draw do
 
   resources :restaurants, only: [:index, :show, :new, :create, :edit, :update] do
     resources :bookings, only: [:create]
-  end
+    get "webmanifest"    => "pwa#manifest"
+  get "service-worker" => "pwa#service_worker"
+end
 
   resources :bookings, only: [] do
     member do
       patch :accept
       patch :decline
-    end
-  end
+      get "webmanifest"    => "pwa#manifest"
+  get "service-worker" => "pwa#service_worker"
+end
+    get "webmanifest"    => "pwa#manifest"
+  get "service-worker" => "pwa#service_worker"
+end
 
   get "search", to: "restaurants#search"
 
   get "/dashboard", to: "pages#dashboard"
 
   root "restaurants#index"
+  get "webmanifest"    => "pwa#manifest"
+  get "service-worker" => "pwa#service_worker"
 end
